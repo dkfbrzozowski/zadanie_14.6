65a2bb08561d1dfede6b599d87a466661f9a104e
Dawid Brzozowski <d.brzozowski@wp.pl>
2018-01-17 07:48:28 +0100

    Add more counters

--- script.js
+++ script.js
@@ -11,13 +11,48 @@ var Counter = React.createClass({
 		});
 	},
 
-	render: function() {
-		return React.createElement('div', {onClick: this.increment},
-			React.createElement('span', {}, 'Licznik ' + this.state.counter)
-		);
-	}
+	decrement: function() {
+		this.setState({
+			counter: this.state.counter - 1
+		});
+	},
+/*
+	componentWillMount: function() {
 
+	},
+
+	componentDidMount: function() {
+
+	},
+
+	schouldComponentUpdate: function() {
+
+	},
+
+	componentWillUpdate: function() {
+
+	},
+
+	componentDidUpdate: function() {
+
+	},
+*/
+	render: function() {
+		return React.createElement('div', {},
+			React.createElement('button', {onClick: this.increment}, 'Zwiększ'),
+			React.createElement('button', {onClick: this.decrement}, 'Zmniejsz'),
+			React.createElement('span', {}, this.state.counter)
+			);
+	}
 });
 
-var element = React.createElement(Counter);
+var element = React.createElement('div', {},
+    React.createElement(Counter, {}),
+    React.createElement(Counter, {}),
+    React.createElement(Counter, {}),
+    React.createElement(Counter, {}),
+    React.createElement(Counter, {})
+);
+
+//var element = React.createElement(Counter);
 ReactDOM.render(element, document.getElementById('app'));
